b95e1d2ed2701144257cb83da70915eab8ef69b9
Cody-Hayes97 <hayesmichael2@gmail.com>
2019-10-24 20:26:35 -0700

    styling before cards on account page

--- how-to-app/src/Components/Dashboard-page/Dashboard.js
+++ how-to-app/src/Components/Dashboard-page/Dashboard.js
@@ -2,11 +2,14 @@ import React, { useContext } from "react";
 import { UserContext } from "../../contexts/index";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import { Container, Segment } from "semantic-ui-react";
-import SearchField from '../SearchResults/SearchField';
+import SearchField from "../SearchResults/SearchField";
+import SearchIcon from "@material-ui/icons/Search";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import { IconButton } from "@material-ui/core";
 
 const Dashboard = props => {
   const [user] = useContext(UserContext);
-  const localUser = JSON.parse(localStorage.user)
+  const localUser = JSON.parse(localStorage.user);
 
   // return (
   //   <>
@@ -19,22 +22,43 @@ const Dashboard = props => {
 
   return (
     <Container>
-      <Container>
-        {localUser.username}
-      </Container>
-      <Segment>
-        Badges and social links
-      </Segment>
-      <Container>
-        <SearchField history={props.history}/>
-      </Container>
-      <Container>
-        My How-To's and Favorites
-      </Container>
-      
-      <Link to='/createpost'> Create Guide</Link>
+      <div className="profile-div">
+        <img
+          className="account-img"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQQJR6BqqLi6y004j182y-DQqexGNssQn5AHlZ7DUBXpYQe3H7P"
+        />
+        <h2 className="user-name">{localUser.username}</h2>
+      </div>
+      <div className="cont-cont">
+        <div className="badges-cont">
+          <h2 className="badges-earned">Badges Earned</h2>
+        </div>
+      </div>
+      <div className="search-field-dash">
+        <SearchField
+          history={props.history}
+          header={<h2 className="find-how-to">Find a How-To</h2>}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment>
+                <IconButton>
+                  <SearchIcon />
+                </IconButton>
+              </InputAdornment>
+            )
+          }}
+        />
+      </div>
+
+      <button className="create-guide-button">
+        <Link to="/createpost" className="link-create-guide">
+          Create a new How-To
+        </Link>
+      </button>
+
+      <Container>My How-To's and Favorites</Container>
     </Container>
-  )
+  );
 };
 
 export default Dashboard;
--- how-to-app/src/Components/SearchResults/SearchField.js
+++ how-to-app/src/Components/SearchResults/SearchField.js
@@ -1,46 +1,46 @@
-import React, { useContext, useState } from 'react'
-import { Form, Segment } from 'semantic-ui-react'
+import React, { useContext, useState } from "react";
+import { Form, Segment } from "semantic-ui-react";
 
-import { useFormInput } from '../../utils/hooks';
-import { GuidesContext } from '../../contexts/index';
+import { useFormInput } from "../../utils/hooks";
+import { GuidesContext } from "../../contexts/index";
 
 const SearchField = props => {
-
-  const [guides, setGuides] = useContext(GuidesContext)
+  const [guides, setGuides] = useContext(GuidesContext);
 
   const [filters, setFilters] = useState([]);
 
   const initialValues = {
-    searchValue: '',
+    searchValue: "",
     searchResults: guides
-  }
+  };
 
-  const [values, changeHandler] = useFormInput(initialValues)
+  const [values, changeHandler] = useFormInput(initialValues);
 
   const searchHandler = () => {
-    setFilters([...filters, values.searchValue])
-    const filteredResults = guides.filter(guide => guide.title.includes(values.searchValue))
-    setGuides(filteredResults)
-    props.history.push('/searchresults')
-  }
+    setFilters([...filters, values.searchValue]);
+    const filteredResults = guides.filter(guide =>
+      guide.title.includes(values.searchValue)
+    );
+    setGuides(filteredResults);
+    props.history.push("/searchresults");
+  };
 
   return (
-    <Segment>
-      <Form
-        onSubmit={searchHandler}
-      >
-        <Form.Input
+    <div>
+      <form className="component-search" onSubmit={searchHandler}>
+        <label htmlFor="searchValue">{props.header}</label>
+        <input
+          className="search-form-input"
           fluid
-          label='Search'
-          name='searchValue'
-          placeholder='Search'
+          label="Search"
+          name="searchValue"
           value={values.searchValue}
           onChange={changeHandler}
         />
-        <Form.Button>Search Guides</Form.Button>
-      </Form>
-    </Segment>
-  )
-}
+        {/* <Form.Button>Search Guides</Form.Button> */}
+      </form>
+    </div>
+  );
+};
 
-export default SearchField
+export default SearchField;
