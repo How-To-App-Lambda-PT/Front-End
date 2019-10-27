f4a00b61f6767f3c37e436f4ab2ecfa0d408da5c
Veto Ramirez <vetorobert3@gmail.com>
2019-10-27 15:03:34 -0700

    added image to page

--- how-to-app/src/Components/CreateAccount/CreateAccount.js
+++ how-to-app/src/Components/CreateAccount/CreateAccount.js
@@ -16,15 +16,18 @@ function CreateAccount({errors, touched, values,}, props) {
        <h4 className="login-logo">How-To</h4>
       </header>
 
-      <Container className="login-cont">
+      <Container className="create-cont">
 
+        <Container>
         <Form>
-          <div className='email'>
-            {/* {touched.email && errors.email && <p>{errors.email}</p>}  */}
-              <Label>Email</Label>
-          </div>
+          <div className="email-cont">
+            <div className='email-text'>
+              {/* {touched.email && errors.email && <p>{errors.email}</p>}  */}
+                <Label>Email</Label>
+            </div>
           <div>
-            <Field type="email" name="username" />
+            <Field className="email-text" type="email" name="username" />
+          </div>
           </div>
 
           <div className='password'>
@@ -48,6 +51,11 @@ function CreateAccount({errors, touched, values,}, props) {
             </Link>             
           </div>
         </Form>
+        </Container>
+
+        <Container>
+          <img className="img-hanger" src="https://s3-alpha-sig.figma.com/img/e3e4/c997/7b42a1a7351c0d5ec11ba9703739e543?Expires=1572825600&Signature=KVxMA1MpqpQz8p2BnuGfi9dcsDbw2CBtR-fSZj32PZ0-0MnTH9jfJMCNUO4dySmyPit2Csk399d4ruT52mvCWiAUEh3WYVeUAtIxjV6I~lnt6MaIXaZkk0LLcxgtO5HdIwj7sIzsEmgeACXNC5QadoTzIZQUZNc2Qs5ywQACpW6rRDPPUpcYIHNCMWojg8woqbKRHM4Z0miCTQqosOLTSt9xaK9dKsiKMezkUz7i~gKTjROOh6rI2J6c6Jb8UlpKv6Tge3gCgcUgNtKGw7~GW-vnxlh4UDmjVrcjtrBlGArfIB~xJ07Qp9AHB0MIAaRwspCa6ml8OyQBY5LVeKBr5A__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA" />
+        </Container>
 
       </Container>
 
