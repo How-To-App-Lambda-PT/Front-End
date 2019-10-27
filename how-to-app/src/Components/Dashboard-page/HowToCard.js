c4ef42bd7225226da41f44b860ff5f2da3b8e13c
Cody-Hayes97 <hayesmichael2@gmail.com>
2019-10-26 23:16:37 -0700

    styling still

--- how-to-app/src/Components/Dashboard-page/HowToCard.js
+++ how-to-app/src/Components/Dashboard-page/HowToCard.js
@@ -1,86 +1,96 @@
-import React, { useContext } from 'react'
-import { Card, Image, Table, Button } from 'semantic-ui-react'
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
-import { axiosWithAuth } from '../../utils/axiosWithAuth'
-import { UserContext } from '../../contexts/index'
-import StarRatingComponent from 'react-star-rating-component'
+import React, { useContext } from "react";
+import { Card, Image, Table, Button } from "semantic-ui-react";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import { axiosWithAuth } from "../../utils/axiosWithAuth";
+import { UserContext } from "../../contexts/index";
+import StarRatingComponent from "react-star-rating-component";
 
 const HowToCard = props => {
-  const guide = props.guide
+  const guide = props.guide;
   const [user] = useContext(UserContext);
 
-    const deleteHowTo = e => {
-    
-    axiosWithAuth('delete', `https://bw-how-to.herokuapp.com/guides/${guide.id}`)
-    .then(res=>console.log(res))
-    .catch(err=>console.log(err))
-  }
-
+  const deleteHowTo = e => {
+    axiosWithAuth(
+      "delete",
+      `https://bw-how-to.herokuapp.com/guides/${guide.id}`
+    )
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  };
 
   const linkHandler = () => {
-    localStorage.setItem('guideId', props.guide.id)
-  }
+    localStorage.setItem("guideId", props.guide.id);
+  };
 
-  if(props.type == 'newsfeed'){
+  if (props.type == "newsfeed") {
     return (
-      <Table className='newsfeed_how_to_card'
-        onClick={linkHandler}
-      >
+      <Table className="newsfeed_how_to_card" onClick={linkHandler}>
         <Table.Row>
-          <Table.Cell collapsing={true}>        
+          <Table.Cell collapsing={true}>
             <Image
-            textAlign='left'
-            size="medium"
-            src="https://react.semantic-ui.com/images/avatar/large/steve.jpg"
+              textAlign="left"
+              size="medium"
+              src="https://react.semantic-ui.com/images/avatar/large/steve.jpg"
             />
           </Table.Cell>
 
-          <Table.Cell verticalAlign='bottom'>
-            <p><span className='green'>Tried it:</span> <span> 0</span></p>
-            <p><span className='green'>Category:</span> <span> {guide.type}</span></p>
-            <p><span className='green'>Difficulty:</span> <span> Easy</span></p>
-            <p><span className='green'>Creator:</span> <span> {guide.username}</span></p>
+          <Table.Cell verticalAlign="bottom">
+            <p>
+              <span className="green">Tried it:</span> <span> 0</span>
+            </p>
+            <p>
+              <span className="green">Category:</span>{" "}
+              <span> {guide.type}</span>
+            </p>
+            <p>
+              <span className="green">Difficulty:</span> <span> Easy</span>
+            </p>
+            <p>
+              <span className="green">Creator:</span>{" "}
+              <span> {guide.username}</span>
+            </p>
           </Table.Cell>
-          
-          <Table.Cell verticalAlign='top' textAlign='left'>
-              <Table.Row><Table.Cell><h2 className='green'>{guide.title}</h2></Table.Cell></Table.Row>
-              <Table.Row>
-              <Table.Cell textAlign='center'>
+
+          <Table.Cell verticalAlign="top" textAlign="left">
+            <Table.Row>
+              <Table.Cell>
+                <h2 className="green">{guide.title}</h2>
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell textAlign="center">
                 <div>
-                  <h2><StarRatingComponent 
-                    name="rate1" 
-                    editing={false}
-                    starCount={5}
-                    value={4.7}
-                  />
+                  <h2>
+                    <StarRatingComponent
+                      name="rate1"
+                      editing={false}
+                      starCount={5}
+                      value={4.7}
+                    />
                   </h2>
                 </div>
               </Table.Cell>
-              </Table.Row>
+            </Table.Row>
           </Table.Cell>
 
-          <Table.Cell textAlign='center'>
-            <button >Try me</button>
-
+          <Table.Cell textAlign="center">
+            <button className="try-it">Try It</button>
           </Table.Cell>
         </Table.Row>
       </Table>
-    )
-  } else if(props.type == 'searchResult'){
-    return(
+    );
+  } else if (props.type == "searchResult") {
+    return (
       // <Link to={`/guides/${props.id}`}>
       <Table>
-        <Table.Row className= {(props.i%2) == 1 ? 'markedResult' : ''}>
-
-          <Table.Cell width='7'>
-            {guide.title}
-          </Table.Cell>
+        <Table.Row className={props.i % 2 == 1 ? "markedResult" : ""}>
+          <Table.Cell width="7">{guide.title}</Table.Cell>
 
           <Table.Cell>
             <div>
               <h2>
-                <StarRatingComponent 
-                  name="rate1" 
+                <StarRatingComponent
+                  name="rate1"
                   editing={false}
                   starCount={5}
                   value={4.7}
@@ -89,59 +99,72 @@ const HowToCard = props => {
             </div>
           </Table.Cell>
 
-          <Table.Cell textAlign='right'>
+          <Table.Cell textAlign="right">
             <p>Tried: 64 times</p>
           </Table.Cell>
-
         </Table.Row>
       </Table>
       // </Link>
-    )
-  } else if(props.type == 'account'){
-    return(
-      <Table className='newsfeed_how_to_card'
-        onClick={linkHandler}
-      >
+    );
+  } else if (props.type == "account") {
+    return (
+      <Table className="newsfeed_how_to_card" onClick={linkHandler}>
         <Table.Row>
-          <Table.Cell collapsing={true}>        
+          <Table.Cell collapsing={true}>
             <Image
-            textAlign='left'
-            size="small"
-            src="https://react.semantic-ui.com/images/avatar/large/steve.jpg"
+              textAlign="left"
+              size="small"
+              src="https://react.semantic-ui.com/images/avatar/large/steve.jpg"
             />
           </Table.Cell>
 
-          <Table.Cell verticalAlign='bottom'>
-            <p><span className='green account_card'>Tried it:</span> <span> 0</span></p>
-            <p><span className='green account_card'>Category:</span> <span> {guide.type}</span></p>
-            <p><span className='green account_card'>Difficulty:</span> <span> Easy</span></p>
-            <p><span className='green account_card'>Creator:</span> <span> {guide.username}</span></p>
+          <Table.Cell verticalAlign="bottom">
+            <p>
+              <span className="green account_card">Tried it:</span>{" "}
+              <span> 0</span>
+            </p>
+            <p>
+              <span className="green account_card">Category:</span>{" "}
+              <span> {guide.type}</span>
+            </p>
+            <p>
+              <span className="green account_card">Difficulty:</span>{" "}
+              <span> Easy</span>
+            </p>
+            <p>
+              <span className="green account_card">Creator:</span>{" "}
+              <span> {guide.username}</span>
+            </p>
           </Table.Cell>
-          
-          <Table.Cell verticalAlign='top' textAlign='left'>
-              <Table.Row><Table.Cell><h4 className='green'>{guide.title}</h4></Table.Cell></Table.Row>
-              <Table.Row>
-              <Table.Cell textAlign='center'>
+
+          <Table.Cell verticalAlign="top" textAlign="left">
+            <Table.Row>
+              <Table.Cell>
+                <h4 className="green">{guide.title}</h4>
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell textAlign="center">
                 <div>
-                  <StarRatingComponent 
-                    name="rate1" 
+                  <StarRatingComponent
+                    name="rate1"
                     editing={false}
                     starCount={5}
                     value={4.7}
                   />
                 </div>
               </Table.Cell>
-              </Table.Row>
+            </Table.Row>
           </Table.Cell>
 
-          <Table.Cell textAlign='center'>
-            <Button icon='edit'></Button>
-            <Button icon='trash' onClick={deleteHowTo}></Button>
+          <Table.Cell textAlign="center">
+            <Button icon="edit"></Button>
+            <Button icon="trash" onClick={deleteHowTo}></Button>
           </Table.Cell>
         </Table.Row>
       </Table>
-    )
+    );
   }
-}
+};
 
-export default HowToCard
+export default HowToCard;
--- how-to-app/src/Components/HowToGuide/HowToGuide.js
+++ how-to-app/src/Components/HowToGuide/HowToGuide.js
@@ -1,93 +1,93 @@
-import React, { useEffect, useState } from 'react'
-import { Segment, Header, Card, Container, Embed } from 'semantic-ui-react';
-import { axiosWithAuth } from '../../utils/axiosWithAuth';
+import React, { useEffect, useState } from "react";
+import { Segment, Header, Card, Container, Embed } from "semantic-ui-react";
+import { axiosWithAuth } from "../../utils/axiosWithAuth";
 
 const style = {
   h1: {
-    marginTop: '3em',
+    marginTop: "3em"
   },
   h2: {
-    margin: '4em 0em 2em',
+    margin: "4em 0em 2em"
   },
   h3: {
-    marginTop: '2em',
-    padding: '2em 0em',
+    marginTop: "2em",
+    padding: "2em 0em"
   },
   last: {
-    margin: '80px',
-  },
-}
+    margin: "80px"
+  }
+};
 
 const HowToGuide = props => {
+  let id = localStorage.getItem("guideId");
 
-  let id = localStorage.getItem('guideId')
+  const [guide, setGuide] = useState();
 
-  const [guide, setGuide] = useState()
-
-  useEffect(() => { 
-    axiosWithAuth('get', `https://bw-how-to.herokuapp.com/guides/${id}`)
+  useEffect(() => {
+    axiosWithAuth("get", `https://bw-how-to.herokuapp.com/guides/${id}`)
       .then(res => setGuide(res.data[0]))
-      .catch(err => console.log('HowToGuide: useEffect: GET:', err))
-  }, [])
+      .catch(err => console.log("HowToGuide: useEffect: GET:", err));
+  }, []);
 
   if (!guide) {
-    return (
-      <div>Loading ...</div>
-    )
+    return <div>Loading ...</div>;
   }
 
   const stepArr = () => {
-    let arr = []
+    let arr = [];
     for (let key in guide) {
-      if (key.toString().includes('step')) {
-        arr.push(key)
+      if (key.toString().includes("step")) {
+        arr.push(key);
       }
     }
-    return arr
-  }
+    return arr;
+  };
 
   const steps = stepArr().map(step => {
-    const titleNumber = step.split('_')[1]
+    const titleNumber = step.split("_")[1];
     return (
       <Card
         key={titleNumber}
         header={`Step ${titleNumber}`}
         description={guide[`${step}`]}
       />
-    )
-  })
+    );
+  });
 
-  const videoLink = guide.link || ''
-  
-  const videoId = videoLink.split('=')[1] || ''
+  const videoLink = guide.link || "";
+
+  const videoId = videoLink.split("=")[1] || "";
 
   const videoPlayer = (
     <Embed
       style={style.last}
       id={videoId}
-      source='youtube'
+      source="youtube"
       autoplay={true}
       brandedUI
     />
-  )
-
-
+  );
 
   return (
-    <Segment>
+    <Segment style={{ padding: 0 }}>
       <Container>
-        <Header as='h1' style={style.h1} textAlign='center' content={guide.title} />
-        <Header as='h3' style={style.h3} textAlign='center' content={guide.description} />
-        <Card.Group itemsPerRow={1}>
-          {steps}
-        </Card.Group>
+        <Header
+          as="h1"
+          style={style.h1}
+          textAlign="center"
+          content={guide.title}
+        />
+        <Header
+          as="h3"
+          style={style.h3}
+          textAlign="center"
+          content={guide.description}
+        />
+        <Card.Group itemsPerRow={1}>{steps}</Card.Group>
       </Container>
-      <Container>
-        {videoPlayer || 'No Video Available'}
-      </Container>
+      <Container>{videoPlayer || "No Video Available"}</Container>
     </Segment>
-  )
+  );
+};
 
-}
-
-export default HowToGuide
+export default HowToGuide;
