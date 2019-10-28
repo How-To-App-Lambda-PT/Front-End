7b97ace2697cf6daa48b2d50dadcd60e793497b8
Dave Kane <davelkane@gmail.com>
2019-10-28 00:59:19 -0500

    ()*@&^#)*(@&^#*)

--- how-to-app/src/PrivateRoute.js
+++ how-to-app/src/PrivateRoute.js
@@ -4,6 +4,7 @@ import { Route, Redirect } from 'react-router-dom'
 const PrivateRoute = ({ component: Component, ...rest }) => {
   
   const token = localStorage.token
+  console.log('PrivetRoute: token=', token)
   return (
     <Route
       {...rest} render={props => token ? <Component {...props} {...rest} /> 
