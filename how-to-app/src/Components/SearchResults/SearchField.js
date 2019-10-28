4b9bb72b4bb5ddcdcc86d426d8b706eb1e46906a
Cody-Hayes97 <hayesmichael2@gmail.com>
2019-10-27 19:08:21 -0700

    finished styling on my account page

--- how-to-app/src/Components/SearchResults/SearchField.js
+++ how-to-app/src/Components/SearchResults/SearchField.js
@@ -1,46 +1,49 @@
-import React, { useContext } from 'react'
-import { Form } from 'semantic-ui-react'
+import React, { useContext } from "react";
+import { Form } from "semantic-ui-react";
 
-import { useFormInput } from '../../utils/hooks';
-import { GuidesContext } from '../../contexts/index';
+import { useFormInput } from "../../utils/hooks";
+import { GuidesContext } from "../../contexts/index";
 
 const SearchField = props => {
-
-  const [guides, setGuides] = useContext(GuidesContext)
+  const [guides, setGuides] = useContext(GuidesContext);
 
   const initialValues = {
-    searchValue: '',
+    searchValue: "",
     searchResults: guides
-  }
+  };
 
-  const [values, changeHandler] = useFormInput(initialValues)
+  const [values, changeHandler] = useFormInput(initialValues);
 
   const searchHandler = () => {
+    const filteredResults = guides.filter(guide =>
+      guide.title.includes(values.searchValue)
+    );
 
-    const filteredResults = guides.filter(guide => guide.title.includes(values.searchValue))
+    setGuides(filteredResults);
 
-    setGuides(filteredResults)
-
-    localStorage.setItem('searchTerm', values.searchValue)
-    localStorage.setItem('guides', JSON.stringify(filteredResults))
-    console.log('SearchField: searchHandler: filteredResults=', filteredResults)
-    props.history.push('/searchresults')
-  }
+    localStorage.setItem("searchTerm", values.searchValue);
+    localStorage.setItem("guides", JSON.stringify(filteredResults));
+    console.log(
+      "SearchField: searchHandler: filteredResults=",
+      filteredResults
+    );
+    props.history.push("/searchresults");
+  };
 
   return (
     <Form onSubmit={searchHandler}>
-        <Form.Input
-          onSubmit={searchHandler}
-          fluid
-          name='searchValue'
-          placeholder='Search'
-          value={values.searchValue}
-          onChange={changeHandler}
-          icon="search"
-          iconPosition='left'
-        />
-        </Form>
-  )
-}
+      <Form.Input
+        className="searchfield"
+        onSubmit={searchHandler}
+        fluid
+        name="searchValue"
+        value={values.searchValue}
+        onChange={changeHandler}
+        icon="search"
+        iconPosition="left"
+      />
+    </Form>
+  );
+};
 
-export default SearchField
+export default SearchField;
